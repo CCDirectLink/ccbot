790145665833a322fa69c2f5c1499f9e5d885849
20kdc <asdd2808@gmail.com>
2019-07-09 09:01:20 +0100

    Port some old behaviors

--- doc/TODO.md
+++ doc/TODO.md
@@ -4,4 +4,14 @@ missing commands from general: poll, leaCheeseArmy, purge, box, rbox, setname, s
 
 other modules status unknown
 
-old behaviors (index.js "?release" "failed to load" ) need reimplementing!
+old behaviors need reimplementing!
+
+countdown isn't there (should be separate entity really)
+
+"failed to load" -> "oof" is done
+
+stream drawing monitor isn't done, but a skeleton's in the old behaviors entity
+
+member enter/leave & roles control is completely not there.
+
+part of this is OldBehaviors territory, part of this should be the roles commands
--- src/entities/old-behaviors.ts
+++ src/entities/old-behaviors.ts
@@ -1,12 +1,39 @@
+import * as discord from 'discord.js';
 import {CCBotEntity, CCBot} from '../ccbot';
 import {EntityData} from '../entity-registry';
+import {silence, channelAsTBF} from '../utils';
 
 /**
  * Implements old behaviors into the bot.
  */
 class OldBehaviorsEntity extends CCBotEntity {
+    private messageListener: (m: discord.Message) => void;
+    
     public constructor(c: CCBot, data: EntityData) {
         super(c, 'old-behaviors-manager', data);
+        this.messageListener = (m: discord.Message) => {
+            if (this.killed)
+                return;
+
+            const lowerContent = m.content.toLowerCase();
+
+            // Where the actual behaviors are
+            if (lowerContent.startsWith('failed to load')) {
+                silence(m.reply('oof'));
+            } else if (lowerContent.startsWith('?release')) {
+                // Might need to be part of a relevant activity manager
+                silence(m.reply('Watching the final countdown'));
+            } else if ((m.channel as any).name === 'media') {
+                // Yes, the continued requirement of using any to do things is awkward.
+                const cc = m.guild.channels.find('name', 'crosscode');
+            }
+        };
+        this.client.on('message', this.messageListener);
+    }
+    
+    public onKill() {
+        super.onKill();
+        this.client.removeListener('message', this.messageListener);
     }
 }
 
