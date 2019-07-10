ad3390bbbb68f39601b59e7847c87b87a329ff2a
20kdc <asdd2808@gmail.com>
2019-07-10 11:24:27 +0100

    Stop relying on secrets.json when it may not be available (on CI for example)

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 //import * as discordJS from 'discord.js';
 import * as commando from 'discord.js-commando';
-import secrets from './data/secrets';
 import CCBotImpl from './ccbot-impl';
 
 /**
@@ -8,6 +7,8 @@ import CCBotImpl from './ccbot-impl';
  */
 class CCBotMain {
     async init(): Promise<void> {
+        // This file may not exist in Travis.
+        const secrets = globalThis['require']('./data/secrets');
         // See ccbot-impl.ts for more details on what's going on here.
         // Use CCBot to refer to the class.
         const client = new CCBotImpl({
--- tests/main.spec.ts
+++ tests/main.spec.ts
@@ -4,10 +4,10 @@ import DynamicDataManager from '../src/dynamic-data';
 describe('DynamicDataManager', (): void => {
     const ddm: DynamicDataManager = new DynamicDataManager();
     it('should have a well-formed JSON command set', (): void => {
-        const seen: {[name: string]: true} = {};
         // Type-check everything.
         expect(ddm.commands.data.constructor).to.equal(Object);
         for (const group in ddm.commands.data) {
+            const seen: {[name: string]: true} = {};
             expect(group.constructor).to.equal(String);
             expect(ddm.commands.data[group].constructor).to.equal(Object);
             for (const name in ddm.commands.data[group]) {
