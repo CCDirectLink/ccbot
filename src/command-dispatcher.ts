9a60fc6584beea406b0468c8c11ca89df939fcd5
20kdc <asdd2808@gmail.com>
2019-10-04 17:26:00 +0100

    The ability to smite people with bolts of justice!
    Credits:
    Apollo for the idea of it being a fictional command
    Ichiki for bringing up making it a real command

--- src/command-dispatcher.ts
+++ src/command-dispatcher.ts
@@ -134,7 +134,7 @@ class CCBotCommandDispatcher extends (commando.CommandDispatcher as any) {
         const safeGroups: string[] = ['util', 'formatter', 'commands', 'tools'];
         if (rolesState != 'yes')
             safeGroups.push('roles');
-        const safeCommands: string[] = ['general hug', 'general lsemotes', 'general triggered', 'general verytriggered', 'general inspire'];
+        const safeCommands: string[] = ['general hug', 'general lsemotes', 'general triggered', 'general verytriggered', 'general inspire', 'general smite'];
         if (((safeGroups.indexOf(group) == -1) && (safeCommands.indexOf(group + ' ' + command) == -1)) && this.client.sideBySideSafety) {
             if (!command.endsWith('-'))
                 return null;
