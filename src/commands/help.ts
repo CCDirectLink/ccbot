1c9855929c44e835f829e55ed9b761d3225d04bc
ac2pic <ac2pic@gmail.com>
2020-03-07 10:22:55 -0600

    Removed unnecessary text from general commands

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -25,14 +25,19 @@ export default class HelpCommand extends CCBotCommand {
         if (this.groupID == 'general') {
             lines.push('To send a command, prefix the bot prefix (or a ping to the bot).');
             lines.push('The bot prefix/ping isn\'t necessary in DMs.');
-            lines.push('`-general` is optional for these commands.');
             lines.push('');
         }
         
         for (const cmd of this.group.commands.values()) {
             const fmt = cmd.format ? ' ' + cmd.format : '';
-            if (cmd.description != 'UNDOCUMENTED')
-                lines.push('** -' + this.group.id + ' ' + cmd.memberName + fmt + ' **: ' + cmd.description);
+            if (cmd.description != 'UNDOCUMENTED') {
+                if (this.groupID === 'general') {
+                    lines.push('** ' + cmd.memberName + fmt + ' **: ' + cmd.description);
+                } else {
+                    lines.push('** -' + this.group.id + ' ' + cmd.memberName + fmt + ' **: ' + cmd.description);
+                }
+            }
+                
         }
         
         // Append some details on other groups
@@ -68,7 +73,7 @@ export default class HelpCommand extends CCBotCommand {
         } else {
             try {
                 for (const str of text)
-                    await message.author.sendEmbed({description: str}) as discord.Message;
+                    await message.author.send({description: str}) as discord.Message;
                 return await message.say('The help page has been sent to your DMs.');
             } catch (e) {
                 return await message.say('Tried to send help information to DMs, but... are your DMs blocked?');
